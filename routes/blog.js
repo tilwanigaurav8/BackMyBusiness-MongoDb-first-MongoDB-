a8cc30d7285445607077fa2ee51eb180e11f0026
tilwanigaurav8 <tilwanigaurav8@gmail.com>
2023-06-10 16:37:10 +0530

    completed new note

--- routes/blog.js
+++ routes/blog.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const mongodb=require('mongodb');
 const db=require('../data/database');
+
+const ObjectID=mongodb.ObjectId;
 const router = express.Router();
 
 router.get('/', function(req, res) {
@@ -15,5 +18,23 @@ router.get('/new-post', async function(req, res) {
   console.log(authors);
   res.render('create-post',{authors:authors});
 });
+
+router.post('/posts',async function(req,res){
+  const authorId=new ObjectID(req.body.author);
+  const author=await db.getDb.collection('authors').findOne({_id:authorId})
+  const newNote={
+    title:req.body.title,
+    summary:req.body.summary,
+    body:req.body.content,
+    date:new Date(),
+    author:{
+      id:authorId,
+      name:author.name,
+      email:author.email
+    }
+  };
+  const result=await db.getDb().collection('posts').insertOne(newNote);
+  res.redirect('/posts');
+})
 
 module.exports = router;
