20cc122493346b7d4a3610fc2cee1643a9be95f6
tilwanigaurav8 <tilwanigaurav8@gmail.com>
2023-06-11 02:37:53 +0530

    view note

--- routes/blog.js
+++ routes/blog.js
@@ -46,7 +46,25 @@ router.get('/posts/:id',async function(req,res){
   if(!post){
     return res.status(404).render('404');
   }
+  post.humanReadableDate=post.date.toLocaleDateString('en_US',{
+    weekday:'long',
+    year:'numeric',
+    month:'long',
+    day:'numeric'
+  });
+  post.date=post.date.toIsoString();
   res.render('post-detail',{post:post})
+});
+
+router.get('/posts/:id/edit',async function(req,res){
+  const postId=req.params.id;
+  const post=await db.getDb().collection('posts').findOne({_id:new ObjectID(postId)},{title:1,summary:1,body:1});
+
+  if(!post){
+    return res.status(404).render('404');
+  }
+
+  res.render('update-post',{post:post});
 });
 
 module.exports = router;
